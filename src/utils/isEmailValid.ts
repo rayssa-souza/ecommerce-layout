9712d4bd7221938704a0087539634619ab23659c
Rayssa de Souza <rayssasouzaunb@gmail.com>
2023-12-20 15:42:14 -0300

    added type on isEmailValid function (utils)

--- src/utils/isEmailValid.ts
+++ src/utils/isEmailValid.ts
@@ -1,4 +1,4 @@
-const isEmailValid = (email) => {
+const isEmailValid = (email: string) => {
   const emailRegex = new RegExp("[a-z0-9]+@[a-z0-9]+.[a-z]{2,3}");
   const emailValidation = emailRegex.test(email);
   return emailValidation;
